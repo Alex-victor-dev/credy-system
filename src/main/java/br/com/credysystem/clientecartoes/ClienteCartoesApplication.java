56c86929402ccadb4c5e7dfb48d6a89f5fde980b
Alex-Victor <alexvictoribeiro@gmail.com>
2022-09-06 16:35:41 -0300

    init commit

---
+++ src/main/java/br/com/credysystem/clientecartoes/ClienteCartoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.credysystem.clientecartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteCartoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/credysystem/clientecartoes/ClienteCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.credysystem.clientecartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteCartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
